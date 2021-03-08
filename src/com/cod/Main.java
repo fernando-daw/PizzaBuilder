58422250f565d421288df25be2625dc287f75b64
Damian Nogueiras <damiannogueiras@gmail.com>
2021-03-08 19:43:32 +0100

    Añadimos el builder que usaremos para crear las pizzas

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    Pizzas basica = new Pizzas();
-
-	    Pizzas personalizada = new Pizzas(Pizzas.FINA, Pizzas.BIG, true, true, true, Pizzas.PARALLEVAR);
+	    // utilizamos el builder y en concreto el metodo build
+        // para tener nuestra pizza
+	    Pizzas creadaConBuilder = new BuilderPizzas().build();
+	    Pizzas sintaxisEncadenada = new BuilderPizzas()
+                .setCebolla(true)
+                .setSize(Pizzas.BIG)
+                .build();
 
 	    // aunque el toSpring no está definida, nos valen estas lineas para poner
         // un punto de ruptura y hacer debug
         // asi podemos ver los objetos creados
-        System.out.println(basica.toString());
-        System.out.println(personalizada.toString());
+        System.out.println(creadaConBuilder.toString());
+        System.out.println(sintaxisEncadenada.toString());
     }
 }
